833106f1e071dd41f366dcc13ea6259cef5fcb3a
VyaliMuteka <vyali17@gmail.com>
2019-02-28 14:27:06 +0000

    Criei meu primeiro repositorio

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+    <meta charset="UTF-8"/>
+
+    <title> </title>
+
+    <style>
+
+
+
+    </style>
+</head>
+
+<body>
+
+
+<?php
+
+echo"Hello word";
+
+?>
+
+</body>
+
+</html>
